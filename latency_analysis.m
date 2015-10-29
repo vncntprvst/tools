b78cf80c9ccebbf94405998a4998af0c51ea2ad0
Vincent <vincent@server.com>
2015-10-29 12:59:08 -0400

    eye vel plots

--- latency_analysis.m
+++ latency_analysis.m
@@ -1,6 +1,6 @@
 function pkOffsets=latency_analysis(behavData,neuralData)
 
-% Latency Analysis
+%% Latency Analysis
 %
 % We used an alignment algorithm to find a relative temporal offset for the
 % neural and behavior data on each trial as follows. Single-trial PETHs
@@ -36,32 +36,80 @@ function pkOffsets=latency_analysis(behavData,neuralData)
 % obtain z-scored population PETHs.
 %
 
-[pkOffsets,peth]=deal(cell(size(neuralData,1),1));
+%% recordings
+[pkOffsets]=cell(size(neuralData,1),1);
+peth=cell(size(neuralData,1),2);
 sigma=200;
 for rec=1:size(neuralData,1)
-    for trial=1:size(neuralData{rec,2},1)
-        trialconv = fullgauss_filtconv(neuralData{rec,2}(trial,neuralData{rec,3}-(1000+3*sigma):neuralData{rec,3}+3*sigma+199),sigma,1).*1000;
+    for trial=1:size(neuralData{rec,2,2},1)
+        % trials aligned to sac
+        % trialconv = fullgauss_filtconv(neuralData{rec,2}(trial,neuralData{rec,3}-(1000+3*sigma):neuralData{rec,3}+3*sigma+199),sigma,1).*1000;
+        % trials aligned to tgt
+        trialconv = fullgauss_filtconv(neuralData{rec,2,2}(trial,...
+            neuralData{rec, 4,2}{1, trial}(1)-(400+3*sigma):... % 400ms before tgt
+            neuralData{rec, 4,2}{1, trial}(1)+3*sigma+799),sigma,1).*1000;    % 800 ms after  
         peth{rec,1}(trial,:) = trialconv(1:10:end);%downsample
     end
-    [sortedRT,sortedRTidx]=sort(behavData{rec, 3})
-    peth{rec,1}=peth{rec,1}(sortedRTidx,:);
-    sortedTgt=cellfun(@(x) x(1),neuralData{rec, 4}(sortedRTidx))-(neuralData{rec,3}-1000);    
-    sortedSac=sortedRT+sortedTgt;
+    % sort trials by RT
+    [sortedRT,sortedRTidx]=sort(behavData{rec, 3});% sort reation times
+    peth{rec,1}=peth{rec,1}(sortedRTidx,:);        % sort PETHs accordingly
+    % if aligned to saccades:
+    %     sortedTgt=cellfun(@(x) x(1),neuralData{rec, 4}(sortedRTidx))-(neuralData{rec,3}-1000);    
+    %     sortedSac=sortedRT+sortedTgt;
+    % aligned to tgt:
+    sortedSac=sortedRT+400;
+    sortedTgt=sortedSac-sortedRT; 
     sortedTgt=round(sortedTgt/10);%downsample
     sortedSac=round(sortedSac/10);%downsample
 end
 
-% sort trials by RT
- 
+%% plotting rec trials
+% figure; hold on;
+% imagesc(1:size(peth{rec,1},2),1:size(peth{rec,1},1),peth{rec,1});
+% colormap(parula)
+% tgth=plot(sortedTgt,1:size(peth{rec,1},1),'Marker','s','MarkerSize',4,'MarkerEdgeColor','k','MarkerFaceColor',[0.7, 0.9, 1]);
+% sach=plot(sortedSac,1:size(peth{rec,1},1),'Marker','.','MarkerFaceColor',[0, 0.5, 0.1]);
+% cbh=colorbar;
+% cbh.Label.String = 'Firing rate (Hz)';
+% set(gca,'xticklabel',-200:200:800,'TickDir','out','FontSize',10); %'xtick',1:100:max ...
+% xlabel('Time, aligned to saccade')
+% ylabel('Trial # - Trials sorted by reaction time')
+% legend([tgth sach],{'Target On', 'Saccade Onset'},'location','Southeast')
+% title('Eye velocity - Native alignement')
+% axis('tight');
+
+%% eye movement plots
+ev_peth=cell(size(behavData,1),2);
+for rec=1:size(behavData,1)
+    for trial=1:size(behavData{rec,5},1)
+%         trialconv = fullgauss_filtconv(behavData{rec,5}(trial,...
+%             neuralData{rec, 4,2}{1, trial}(1)-(400+3*sigma):... % 400ms before tgt
+%             neuralData{rec, 4,2}{1, trial}(1)+3*sigma+799),sigma,1).*1000;    % 800 ms after  
+        trial_ev = behavData{rec,5}(trial,...
+            neuralData{rec, 4,2}{1, trial}(1)-400:... % 400ms before tgt
+            neuralData{rec, 4,2}{1, trial}(1)+799).*1000;    % 800 ms after  
+        ev_peth{rec,1}(trial,:) = trial_ev(1:10:end);%downsample
+    end
+    % sort trials by RT
+    [~,sortedRTidx]=sort(behavData{rec, 3});        % sort reation times
+    ev_peth{rec,1}=ev_peth{rec,1}(sortedRTidx,:);   % sort ev_PETHs accordingly
+end
+
+%% plotting eye vel trials
 figure; hold on;
-imagesc(1:size(peth{rec,1},2),1:size(peth{rec,1},1),peth{rec,1});
-tgth=plot(sortedTgt,1:size(peth{rec,1},1),'Marker','d','MarkerEdgeColor','k','MarkerFaceColor',[0.9, 1, 0.7]);
-plot(sortedSac,1:size(peth{rec,1},1),'Marker','.','MarkerEdgeColor','k','MarkerFaceColor',[1, 0.5, 0.9]);
+imagesc(1:size(ev_peth{rec,1},2),1:size(ev_peth{rec,1},1),ev_peth{rec,1});
+colormap(cool)
+tgth=plot(sortedTgt,1:size(ev_peth{rec,1},1),'Marker','o','MarkerSize',4,'MarkerEdgeColor','k','MarkerFaceColor',[0.7, 0.9, 1]);
+sach=plot(sortedSac,1:size(ev_peth{rec,1},1),'Marker','.','MarkerFaceColor',[0, 0.5, 0.1]);
 cbh=colorbar;
-cbh.Label.String = 'Firing rate (Hz)';
-set(gca,'xticklabel',-800:200:200,'TickDir','out','FontSize',10); %'xtick',1:100:max ...
+cbh.Label.String = 'Eye velocity (Degree/sec)';
+set(gca,'xticklabel',-200:200:800,'TickDir','out','FontSize',10); %'xtick',1:100:max ...
 xlabel('Time, aligned to saccade')
-ylabel('Trial #')
-legend(tgth,'Target On','location','Southwest')
-title('Trials sorted by reaction time')
+ylabel('Trial # - Trials sorted by reaction time')
+legend([tgth sach],{'Target On', 'Saccade Onset'},'location','Southeast')
+title('Neuronal activity - Native alignement')
 axis('tight');
+
+
+
+
