3ca999e628371d826c157b4caa47ccb7fc9f324b
Vincent Prevosto <vincent@server.com>
2015-12-07 00:45:26 -0500

    forgot to save those changes

--- clustering/clus_pop_ROC.m
+++ clustering/clus_pop_ROC.m
@@ -1,13 +1,17 @@
-%function which clustered data
-%input: 
-%ROCarray: ROC data formatted in an mxn matrix, where m equals number of
-%neurons, n = rows of ROC data calculated by running calcAUROC on data
-% clusternumber: intended number of clustered to be created
+%Cluster neurons according to their response profiles
+
+%input: reponse data (cell array of n neurons)
+%       baseline data (cell array of n neurons)
+%       [optional] clusternumber: intended number of clustered to be created
 
 %output: clusterind (indices assigning each row to a cluster)
 %output plot: silhouette analysis on clustered data
 
-function [clusterind] = clusterROC(ROCarray,clusternumber)
-clustind = kmeans(1-corr(ROCarray'),5,'dist','cityblock');
-silhouette(1-corr(cell2mat(temp')'),clust)
+function clusterIdx = clusterROC(respData,baselineData,clusterNumber)
+%concatenate baseline and response data and calculate AUC
+ROCarray=cellfun(@(x,y) calcAUROC([x,y],[1 size(y,2)]),respData,baselineData, 'UniformOutput',false); 
+%ROCarray: ROC data formatted in an mxn matrix, where m equals number of
+%neurons, n = rows of ROC data calculated by running calcAUROC on data
+clusterIdx = kmeans(1-corr(cat(1,ROCarray{:})'),5,'dist','cityblock');
+% silhouette(1-corr(cell2mat(temp')'),clust)
 end
